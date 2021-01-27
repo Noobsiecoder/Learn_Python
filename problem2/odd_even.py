04bb1de53ca46d7f2ac17e1a605c6d1a9e9958da
Abhishek Sriram <68271853+Noobsiecoder@users.noreply.github.com>
2021-01-27 23:40:46 +0530

    Request to update odd_even.py
    This is a python program. Start using pythonic way of coding. The main emphasis on using python to code, is to shorten the coding time. Hence try to make it simple. I didn't get any data from the user as I asked in the question, that's not mandatory. Have fun coding!

--- problem2/odd_even.py
+++ problem2/odd_even.py
@@ -1,5 +1,8 @@
-num= float(input ("Enter number: "))
-if(num%2)==0:
-    print("Number is even")
-else:
-    print("Number is odd")
+def check_odd(num):
+    """ Function to check if a number is even or odd """
+    return "even" if (num % 2) == 0 else "odd"
+
+
+if __name__ == "__main__":
+    print(f'2 is an {check_odd(2)} number')     # 2 is an even number
+    print(f'23 is an {check_odd(23)} number')   # 23 is an odd number
